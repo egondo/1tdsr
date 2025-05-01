4501738e37280dc3115295fdec6199ffea524a36
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-30 22:57:46 -0300

    String e correção de exercicios

---
+++ string/exemplo_acesso.py
@@ -0,0 +1,9 @@
+frase = "Amanha é dia 1º de maio"
+
+print(frase[0]) #imprime o primeiro caracter
+print(frase[10]) #imprime o caracter que esta no indice 10
+
+nome = "   Karlos Tadeu Soares    "
+nome_fmt = nome.strip()
+print(nome_fmt)
+print(nome)
---
+++ string/exemplo_for.py
@@ -0,0 +1,15 @@
+frase = "Every journey starts with a single step"
+
+for letra in frase:
+    print(letra)
+
+for numero in range(200, 300):
+    print(numero)
+
+#contagem regressiva
+for numero in range(10, 0, -1):
+    print(numero)
+
+frase = "Amanha é dia de folga"
+resultado = frase.find("Palmeiras")
+print(resultado)
