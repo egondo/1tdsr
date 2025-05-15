84e47ae0a93ca3848f12139b137073399f8b3a26
Laboratório FIAP <labsfiap@fiap.com.br>
2025-05-14 22:57:25 -0300

    Projeto da enquete

---
+++ projeto_enquete/enquete.py
@@ -0,0 +1,60 @@
+def cadastra_pergunta(repositorio: list):
+    numero = int(input("Número: "))
+    enunciado = input("Enunciado: ")
+    tipo = input("Tipo: ")
+    if tipo == 'unica' or tipo == 'multipla':
+        alternativas = []
+        print("Alternativas ou enter para finalizar")
+        num = 1
+        item = input(f"alternativa {num}: ")
+        while item != '':
+            alternativas.append(item)
+            num = num + 1
+            item = input(f"alternativa {num}: ")
+    else:
+        alternativas = None
+    #colocando pergunta no repositorio
+    repositorio.append(numero)
+    repositorio.append(enunciado)
+    repositorio.append(tipo)
+    repositorio.append(alternativas)
+
+def menu() -> int:
+    print("1) Cadasta Pergunta")
+    print("2) Aplica enquete")
+    print("3) Visualiza perguntas")
+    print("4) Apaga pergunta")
+    print("5) Sair")
+    return int(input("opção: "))
+
+def apaga_pergunta(repositorio):
+    num = int(input("Digite o nº da pergunta que deseja apagar: "))
+    i = 0
+    while i < len(repositorio) and repositorio[i] != num:
+        i = i + 4
+    
+    if repositorio[i] == num:
+        repositorio.pop(i)
+        repositorio.pop(i)
+        repositorio.pop(i)
+        repositorio.pop(i)
+    else:
+        print("Nenhuma pergunta encontrada!")
+
+
+
+#main
+perguntas = []
+opcao = 0
+while opcao != 5:
+    opcao = menu()
+    if opcao == 1:
+        cadastra_pergunta(perguntas)
+    elif opcao == 2:
+        print("aplicando a enquete")
+    elif opcao == 3:
+        print(perguntas)
+    elif opcao == 4:
+        apaga_pergunta(perguntas)
+    elif opcao != 5:
+        print("Opção inválida")
