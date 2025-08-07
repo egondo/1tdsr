9ed4cd4ea89745ed39083739e7d2b576a4cb837a
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-06 22:12:35 -0300

    Finalizando jogo de 21

--- jogo21/jogo21.py
+++ jogo21/jogo21.py
@@ -1,12 +1,50 @@
 import baralho as bar
 
+def imprime_mao(mao: list) -> str:
+    
+    resp = ""
+    for carta in mao:
+        if carta[0] == 11:
+            resp = resp + "J "  
+        elif carta[0] == 12:
+            resp = resp + "Q "
+        elif carta[0] == 13:
+            resp = resp + "K "
+        elif carta[0] == 1:
+            resp = resp + "A "
+        else:
+            resp = resp + str(carta[0]) + " "
+
+        if carta[1] == 'espadas':
+            resp = resp + "♠ "
+        elif carta[1] == 'copas':
+            resp = resp + "♥ "
+        elif carta[1] == 'ouros':
+            resp = resp + "♦ "
+        elif carta[1] == 'paus':
+            resp = resp + "♣ " 
+        
+    return resp.strip()
+
 def quer_carta(mao: list) -> bool:
-    print(mao)
+    #print(mao)
+    print(imprime_mao(mao))
+
     resp = input("Quer carta? ")
     if resp == 'S':
         return True
     else:
         return False
+
+def quer_carta_cpu(mao: list) -> bool:
+    pontos = get_pontos(mao)
+    if pontos < 16:
+        return True
+    else:
+        return False
+
+
+
     
 def get_pontos(mao) -> int:
     pontos = 0
@@ -28,12 +66,14 @@ while quer_carta(mao_hum):
     c = bar.compra(deck)
     mao_hum.append(c)
 
-while quer_carta(mao_cpu):
+while quer_carta_cpu(mao_cpu):
     c = bar.compra(deck)
     mao_cpu.append(c)
 
 pontos_hum = get_pontos(mao_hum)
 pontos_cpu = get_pontos(mao_cpu)
+
+print(imprime_mao(mao_cpu))
 
 if pontos_hum > pontos_cpu and pontos_hum <= 21:
     print("Ser humano venceu!")
