559e41fa2e151f9fbaa9ac8539913ab9f8dccefe
Eduardo Gondo <egondo@gmail.com>
2025-11-03 22:40:46 -0300

    Exemplos de streamlit

--- streamlit/tela.py
+++ streamlit/tela.py
@@ -5,7 +5,14 @@ st.header('Cadastro de série:')
 
 titulo = st.text_input("Título")
 
-escolhas = st.radio("Gênero", ['ação', 'aventura', 'comédia', 'drama', 'guerra', 'suspense', 'terror', 'ficção científica', 'fantasia', 'outros'])
+st.subheader("Gêneros: ")
+valor = st.checkbox('ação', key="acao")
+av = st.checkbox("aventura", key="aventura")
+com = st.checkbox("comédia", key="comédia")
+drama = st.checkbox("drama", key="drama")
+guerra = st.checkbox("guerra", key="guerra")
+
+#, 'aventura', 'comédia', 'drama', 'guerra', 'suspense', 'terror', 'ficção científica', 'fantasia', 'outros'])
 
 atores = st.text_area("atores: ")
 
@@ -19,6 +26,8 @@ if st.button('cadastra'):
         "temporadas": temporadas
     }
     st.write(registro)
+    with open("dados.txt", mode="a") as arq:
+        json.dump(registro, input, indent=4)
 
 
 
